114c487a11090c3abf6cdc24398dfc6aaf291303
tamseelaa <tamseelm@metropolia.fi>
2025-04-24 03:05:13 +0300

    update hrv analysis calculations

--- Classes/HRV analysis class.py
+++ Classes/HRV analysis class.py
@@ -1,96 +1,112 @@
-class HRV_analysis:
-	self.meanPPI=meanPPI
-	self.data=data
-	self.PPI=PPI
-	self.SDSD_value=SDSD_value
-	self.SDNN_value=SDNN_value
-	self.peaks=peaks
-	self.samples=samples
-	self.peaks=[]
-	
-		
-	def reading(self):
-		list=[]
-		for i in range(750):
-			list.append(fifo.get())
-			time.sleep(0.001)
-		return list
-	
-	
-	def peaks(self.samples):
-		average=round(sum(self.samples)/len(self.samples))
-		threshold= average + ((max(self.samples)-min(self.samples))/2)
-		for i in samples:
-			if self.samples[i] < threshold and self.samples[i+1] >= threshold:
-				self.peaks.append(i+1)
-		return self.peaks		
-		
-	
-	def ppi(self.peaks):
-        ppi_list = []
-        for i in range(len(self.peaks)-1):
-            ppi = (self.peaks[i+1]-self.peaks[i])*4
-            if ppi > 300 and ppi < 2000:
-                ppi_list.append(ppi)
-        return ppi_list
+import math, array, time
+from filefifo import Filefifo
+filename='xyz.txt'
+fifo = Filefifo(10, name=filename)
+class HRVAnalysis:
+    def __init__(self):
+        self.fifo=fifo
+        self.samples = []
+        self.peaks = []
+        self.PPI = []
+        self.data = []
+        self.meanPPI_value = 0
+        self.SDSD_value = 0
+        self.SDNN_value = 0
+        #self.samples=samples
+        #self.meanPPI_value = 0
+        #self.SDSD_value = 0
+        #self.SDNN_value = 0
 
+    def reading(self):
+        self.samples = []
+        for _ in range(20):
+            self.samples.append(fifo.get())
+            time.sleep(0.001)
+        return self.samples
 
-    def meanPPI(self.data):
-        sumPPI = 0 
-        for i in self.data:
-            sumPPI += i
-        mean_ppi= sumPPI/len(self.data)
-        return round(mean_ppi)
+    def find_peaks(self):
+        average = round(sum(self.samples) / len(self.samples))
+        threshold = average + ((max(self.samples) - min(self.samples)) / 2)
+        for i in range(len(self.samples) - 1):
+            if self.samples[i] < threshold and self.samples[i + 1] >= threshold:
+                self.peaks.append(i + 1)
+        return self.peaks
 
+    def calculate_ppi(self):
+        self.PPI = []
+        for i in range(len(self.peaks) - 1):
+            ppi = (self.peaks[i + 1] - self.peaks[i]) * 4
+            if 300 < ppi < 2000:
+                self.PPI.append(ppi)
+        self.data = self.PPI  # use PPI as data for other calculations
+        return self.PPI
 
-	def meanHR(self.meanPPI):
-		mean_hr= 60000/self.meanPPI
-		return round(hr_mean)
+    def meanPPI(self):
+        if not self.PPI:
+            print("warning: PPI data is empty.")
+            return 0
+        self.meanPPI_value = sum(self.PPI) / len(self.PPI)
+        return round(self.meanPPI_value)
 
+    def meanHR(self):
+        if self.meanPPI_value == 0:
+            self.meanPPI()
+        mean_hr = 60000 / self.meanPPI_value
+        return round(mean_hr)
 
-	def SDNN(self.data, self.mean_ppi):
-		ppi_difference_sum = 0
-		for i in self.data:
-			ppi_difference_sum += (i-self.mean_ppi)**2
-		sdnn = math.sqrt(ppi_difference_sum/(len(self.data)-1))
-		return round(sdnn)
+    def SDNN(self):
+        mean_ppi = self.meanPPI()
+        variance = sum((x - mean_ppi) ** 2 for x in self.data) / (len(self.data) - 1)
+        self.SDNN_value = math.sqrt(variance)
+        return round(self.SDNN_value)
 
+    def RMSSD(self):
+        diffs = [(self.data[i + 1] - self.data[i]) ** 2 for i in range(len(self.data) - 1)]
+        rmssd = math.sqrt(sum(diffs) / (len(self.data) - 1))
+        return round(rmssd)
 
-	def RMSSD(self.data):
-		i = 0
-		ppi_difference = 0
-		while i < len(self.data)-1:
-			ppi_difference += (self.data[i+1]-self.data[i])**2
-			i +=1
-		rmssd = math.sqrt((ppi_difference/(len(self.data)-1)))
-		return round(rmssd)
+    def SDSD(self):
+        pp_diff = [self.data[i + 1] - self.data[i] for i in range(len(self.data) - 1)]
+        first = sum([x ** 2 for x in pp_diff]) / (len(pp_diff) - 1)
+        second = (sum(pp_diff) / len(pp_diff)) ** 2
+        self.SDSD_value = math.sqrt(first - second)
+        return round(self.SDSD_value)
 
+    def SD1(self):
+        if self.SDSD_value == 0:
+            self.SDSD()
+        sd1 = math.sqrt((self.SDSD_value ** 2) / 2)
+        return round(sd1)
 
-	def SDSD(self.data):
-		PP_array = array.array('l')
-		i = 0
-		first_value = 0
-		second_value = 0
-		while i < len(self.data)-1:
-			PP_array.append(int(self.data[i+1])-int(self.data[i]))
-			i += 1
-		i = 0
-		while i < len(PP_array)-1:
-			first_value += float(PP_array[i]**2)
-			second_value += float(PP_array[i])
-			i += 1
-		first = first_value/(len(PP_array)-1)
-		second = (second_value/(len(PP_array)))**2
-		sdsd= math.sqrt(first - second)
-		return round(sdsd)
+    def SD2(self):
+        if self.SDNN_value == 0:
+            self.SDNN()
+        if self.SDSD_value == 0:
+            self.SDSD()
+        sd2 = math.sqrt((2 * (self.SDNN_value ** 2)) - ((self.SDSD_value ** 2) / 2))
+        return round(sd2)
 
+#samples = [
+ #   828, 836, 852, 760, 800, 796, 856, 824, 808, 776, 724, 816,
+#    800, 812, 812, 812, 756, 820, 812, 800
+#]
 
-	def SD1(self.SDSD_value):
-		sd1 = math.sqrt((self.SDSD_value**2)/2)
-		return round(sd1)
+hrv = HRVAnalysis()
+samples = hrv.reading()
+peaks = hrv.find_peaks()
+ppi = hrv.calculate_ppi()
+mean_ppi = hrv.meanPPI()
+mean_hr = hrv.meanHR()
+sdnn = hrv.SDNN()
+rmssd = hrv.RMSSD()
+sdsd = hrv.SDSD()
+sd1 = hrv.SD1()
+sd2 = hrv.SD2()
 
-
-	def SD2(self.SDNN_value, self.SDSD_value):
-		sd2=math.sqrt((2*(self.SDNN_value**2))-((self.SDSD_value**2)/2))
-		return round(sd2)
+print('ppi=',mean_ppi)
+print('hr=',mean_hr)
+print('sdnn=',sdnn)
+print('rmssd',rmssd)
+print('sdsd=',sdsd)
+print('sd1=',sd1)
 
